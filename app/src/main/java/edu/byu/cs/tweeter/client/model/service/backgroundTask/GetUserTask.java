8dceb2cb3658dcd08fa17fec818265b54527797c
colerusson <colerusson2@gmail.com>
2023-11-11 21:28:29 -0700

    implemented api endpoint for getting user

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetUserTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetUserTask.java
@@ -5,6 +5,10 @@ import android.os.Handler;
 
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.request.GetUserRequest;
+import edu.byu.cs.tweeter.model.net.request.LogoutRequest;
+import edu.byu.cs.tweeter.model.net.response.GetUserResponse;
+import edu.byu.cs.tweeter.model.net.response.LogoutResponse;
 
 /**
  * Background task that returns the profile for a specified user.
@@ -12,6 +16,7 @@ import edu.byu.cs.tweeter.model.domain.User;
 public class GetUserTask extends AuthenticatedTask {
     private static final String LOG_TAG = "GetUserTask";
     public static final String USER_KEY = "user";
+    public static final String URL_PATH = "/getuser";
     private final String alias;
     private User user;
 
@@ -22,15 +27,22 @@ public class GetUserTask extends AuthenticatedTask {
 
     @Override
     protected void doTask() {
-        user = getUser();
+        try {
+            GetUserRequest request = new GetUserRequest(getAuthToken(), alias);
+            GetUserResponse response = getServerFacade().getUser(request, URL_PATH);
+
+            if (response.isSuccess()) {
+                user = response.getUser();
+            } else {
+                throw new RuntimeException(response.getMessage());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
     protected void loadSuccessBundle(Bundle msgBundle) {
         msgBundle.putSerializable(USER_KEY, user);
-    }
-
-    private User getUser() {
-        return getFakeData().findUserByAlias(alias);
     }
 }
--- server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
@@ -2,9 +2,11 @@ package edu.byu.cs.tweeter.server.service;
 
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.request.GetUserRequest;
 import edu.byu.cs.tweeter.model.net.request.LoginRequest;
 import edu.byu.cs.tweeter.model.net.request.LogoutRequest;
 import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
+import edu.byu.cs.tweeter.model.net.response.GetUserResponse;
 import edu.byu.cs.tweeter.model.net.response.LoginResponse;
 import edu.byu.cs.tweeter.model.net.response.LogoutResponse;
 import edu.byu.cs.tweeter.model.net.response.RegisterResponse;
@@ -50,6 +52,15 @@ public class UserService {
         User user = getDummyUser();
         AuthToken authToken = getDummyAuthToken();
         return new RegisterResponse(user, authToken);
+    }
+
+    public GetUserResponse getUser(GetUserRequest request) {
+        if (request.getAlias() == null) {
+            throw new RuntimeException("[Bad Request] Missing a username");
+        }
+
+        User user = getFakeData().findUserByAlias(request.getAlias());
+        return new GetUserResponse(user);
     }
 
     /**
